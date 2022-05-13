e2703758e784605a9a553865f6de43a84dbce98e
Flashi <flashimcflash@gmail.com>
2022-05-14 01:00:53 +0200

    changing back to old name causing config issues
    will be fixed
    
    Co-Authored-By: Fuly <37185496+FulytheFox@users.noreply.github.com>

--- MineTrain/src/fr/dariusmtn/minetrain/listeners/BlockBreakListener.java
+++ MineTrain/src/fr/dariusmtn/minetrain/listeners/BlockBreakListener.java
@@ -3,6 +3,7 @@ package fr.dariusmtn.minetrain.listeners;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -22,8 +23,21 @@ public class BlockBreakListener implements Listener {
 		Player player = event.getPlayer();
 		Location bLoc = event.getBlock().getLocation();
 		Block block = event.getBlock();
-		
-		
+
+		for (BlockFace face : BlockFace.values()) {
+
+			// Check if the adjacent block relative to this face is a furnace
+			if (event.getBlock().getRelative(face).getType() == Material.OAK_BUTTON) {
+				// Do what you want
+				if (plugin.getFileManager().isStationButton(bLoc)) {
+					event.setCancelled(true);
+					player.sendMessage("§cThis button is binded to a station!");
+				}
+				break; // This will prevent the above code being run more than once if there are
+						// multiple adjacent furnaces
+			}
+		}
+
 		if (block.getType().equals(Material.OAK_BUTTON)) {
 			if (plugin.getFileManager().isStationButton(bLoc)) {
 				event.setCancelled(true);
